cca5e8718f0cb534a6f12329c0a91c3dbc3da09f
EmranSWE <mdemran.swe@gmail.com>
2022-09-27 21:52:40 +0600

    node and express practice

---
+++ __steps.js
@@ -0,0 +1,16 @@
+/* 
+1. Create  a new folder(manually or mkdir)
+2.Open the folder
+3.npm init
+4.npm install express
+5.create index.js
+-------------
+For express
+
+1.require express
+2.create app variable
+3.declare port
+4.set app.get('/')
+5.node index.js
+6.check your browser for that port
+*/
